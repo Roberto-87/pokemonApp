1f0a9ab81ae2644e9b88358a1cb2eb26519c4454
Roberto Ochoa <oc_005@hotmail.com>
2023-12-15 14:35:10 -0300

    next config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    generateBuildId: async () => {
+      // This could be anything, using the latest git hash
+      return process.env.GIT_HASH
+    },
+  }
